7a605f7812e25a9c3e92996f0b38682954a160c0
youngj <youngj@15eebb39-2e09-4086-82a0-f2f0cd2888cc>
2010-01-04 03:33:24 +0000

    add organizations
    git-svn-id: http://anvaya.unfuddle.com/svn/anvaya_anvaya@3 15eebb39-2e09-4086-82a0-f2f0cd2888cc

---
+++ mod/anvaya/languages/en.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * Elgg groups plugin language pack
+	 *
+	 * @package ElggGroups
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.com/
+	 */
+
+	$english = array(
+
+			'org:name' => 'Group name',
+			'org:username' => 'Group short name (displayed in URLs, alphanumeric characters only)',
+			'org:description' => 'Description',
+			'org:briefdescription' => 'Brief description',
+			'org:interests' => 'Tags',
+			'org:website' => 'Website',
+			'org:members' => 'Members',
+			'org:owner' => "Owner",
+			'org:location' => 'Location',
+
+			'item:group:organization' => "Organizations",
+	);
+
+	add_translation("en",$english);
+?>
--- mod/anvaya/start.php
+++ mod/anvaya/start.php
@@ -1,6 +1,31 @@
 <?php
 
+// Class source
+class Organization extends ElggGroup {
+
+  protected function initialise_attributes() {
+    parent::initialise_attributes();
+    $this->attributes['subtype'] = 'organization';
+  }
+
+  public function __construct($guid = null) {
+    parent::__construct($guid);
+  }
+
+  // more customizations here
+}
+
 function pluginname_init() {
+
+	org_fields_setup();
+
+	// Register a page handler, so we can have nice URLs
+	register_page_handler('org','org_page_handler');
+
+	register_entity_type('group', 'organization');
+   	// This operation only affects the db on the first call for this subtype
+   	// If you change the class name, you'll have to hand-edit the db
+   	add_subtype('group', 'organization', 'Organization');
 
     // Extend system CSS with our own styles
     //extend_view('css','pluginname/css');
@@ -9,9 +34,41 @@ function pluginname_init() {
     //register_plugin_hook('index','system','new_index');
 }
 
+/**
+ * Group page handler
+ *
+ * @param array $page Array of page elements, forwarded by the page handling mechanism
+ */
+function org_page_handler($page)
+{
+	global $CONFIG;
+
+	if (isset($page[0]))
+	{
+		set_input('org_guid', $page[0]);
+		include(dirname(__FILE__) . "/orgprofile.php");
+	}
+}
+
+function org_fields_setup()
+{
+	global $CONFIG;
+
+	$CONFIG->org_fields = array(
+		'name' => 'text',
+		'description' => 'longtext',
+		'briefdescription' => 'text',
+		'interests' => 'tags',
+		'website' => 'url',
+		'location' => 'text',
+	);
+}
+
+
+
 function new_index() {
 
-    if (!include_once(dirname(__FILE__)) . "/index.php")
+    if (!include_once(dirname(__FILE__) . "/index.php"))
     	return false;
 
     return true;
