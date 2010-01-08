8c454bc14cb75f1afe1a8758eccc9723b44510b0
jjstern <jjstern@15eebb39-2e09-4086-82a0-f2f0cd2888cc>
2010-01-08 03:00:23 +0000

    editorg file
    git-svn-id: http://anvaya.unfuddle.com/svn/anvaya_anvaya@6 15eebb39-2e09-4086-82a0-f2f0cd2888cc

---
+++ mod/anvaya/editOrg.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	 * Elgg Envaya plugin
+	 * 
+	 * @package ElggGroups
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.com/
+	 */
+
+	require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+	gatekeeper();
+
+	$group_guid = get_input('group_guid');
+	$group = get_entity($group_guid);
+	set_page_owner($group_guid);
+
+	$title = elgg_echo("org:edit");
+	
+	
+	$body = elgg_view_title($title);
+	
+	if (($group) && ($group->canEdit()))
+	{
+		$body .= elgg_view("org/editOrg", array('entity' => $group));
+			 
+	} else {
+		$body .= elgg_view('org/contentwrapper',array('body' => elgg_echo('org:noaccess')));
+	}
+	
+	$body = elgg_view_layout('two_column_left_sidebar', '', $body);
+	
+	page_draw($title, $body);
+?>
