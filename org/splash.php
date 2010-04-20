99f863366b023d0468c7e59e5b6474ef8d66f6b1
jjstern <jjstern@15eebb39-2e09-4086-82a0-f2f0cd2888cc>
2010-04-20 03:21:06 +0000

    updated splash page
    git-svn-id: http://anvaya.unfuddle.com/svn/anvaya_anvaya@129 15eebb39-2e09-4086-82a0-f2f0cd2888cc

--- org/splash.php
+++ org/splash.php
@@ -1,9 +1,29 @@
-<html><body>
-<table height='100%' width='100%'><tr><td align='center' valign='center'>
-<img src='_graphics/splash_logo.png' />
+<html><body style="background:repeat-x url('_graphics/splashBG.png');">
+<table width='100%' style="margin-top:50px;"><tr><td align='center' valign='top' style="font-family:tahoma;font-size:65px;letter-spacing:18px;">
+ENVAYA</tr></td>
+</table>
+    
+    
+<table width='100%' style="margin-top:35px;font-style='tahoma;'">
+    <tr><td width="50%" align='right' valign='top' style="padding-right:35px;">
+<a style="text-decoration:none;" href="/page/home"><img style="border:none;" src='_graphics/splashTZ.png' /></a>
 <br />
-<div style='font:14px Arial'>
-Coming soon...
+<div style='font:17px tahoma;color:#444;margin-right:35px;margin-top:10px;'>
+Envaya Tanzania
 </div>
-</td></tr></table>
+</td>
+
+<td align='left' valign='top' width="50%" style="padding-left:25px;">
+<img src='_graphics/splashGlobe.png' />
+<br />
+<div style='font:17px tahoma;color:#666;margin-left:20px;margin-top:14px;'>
+Envaya International
+</div>
+<br />
+<div style='font:12px tahoma;color:#777;margin-left:55px;margin-top:-10px;'>
+(Coming soon)
+</div>
+</td></tr>
+
+</table>
 </body></html>
