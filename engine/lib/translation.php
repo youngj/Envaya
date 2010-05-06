7c709ee88e2c68fc9075c3e9c74279603bc61a10
youngj <youngj@15eebb39-2e09-4086-82a0-f2f0cd2888cc>
2010-05-06 23:33:54 +0000

    fix google translate for long content
    git-svn-id: http://anvaya.unfuddle.com/svn/anvaya_anvaya@155 15eebb39-2e09-4086-82a0-f2f0cd2888cc

--- engine/lib/translation.php
+++ engine/lib/translation.php
@@ -223,11 +223,13 @@ function get_auto_translation($text, $origLang, $viewLang)
     $text = str_replace("\r","", $text);
     $text = str_replace("\n", ",;", $text);
 
-    $url = "ajax.googleapis.com/ajax/services/language/translate?v=1.0&langpair=$origLang%7C$viewLang&q=".urlencode($text);
+    $url = "ajax.googleapis.com/ajax/services/language/translate?v=1.0&langpair=$origLang%7C$viewLang";
 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_REFERER, "www.envaya.org");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, array('q' => $text));
 
     $json = curl_exec($ch);
 
@@ -254,7 +256,8 @@ function guess_language($text)
 	}
 
 	$ch = curl_init();
-	$url = "ajax.googleapis.com/ajax/services/language/detect?v=1.0&q=".urlencode($text);
+
+	$url = "ajax.googleapis.com/ajax/services/language/detect?v=1.0&q=".urlencode(get_snippet($text, 500));
 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_REFERER, "www.envaya.org");
