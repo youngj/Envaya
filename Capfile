3cb805fea39399c53de3409e62804f3156cc2976
youngj <youngj@15eebb39-2e09-4086-82a0-f2f0cd2888cc>
2010-07-07 21:45:13 +0000

    capistrano deployment scripts
    git-svn-id: http://anvaya.unfuddle.com/svn/anvaya_anvaya@173 15eebb39-2e09-4086-82a0-f2f0cd2888cc

---
+++ Capfile
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'railsless-deploy'
+
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+
+load 'config/deploy' # remove this line to skip loading any of the default tasks
---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+
+set :application, "envaya"
+set :repository,  "http://anvaya.unfuddle.com/svn/anvaya_anvaya"
+set :deploy_to, "/var/envaya"
+set :deploy_via, :remote_cache
+set :copy_exclude, [".svn", "localsettings.php", "selenium-server.jar"]
+
+set :user, "root"
+
+set :scm, :subversion
+set :scm_username, "youngj"
+set :scm_password, "LssP12"
+
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "www.envaya.org"                          # Your HTTP server, Apache/etc
+role :app, "www.envaya.org"                          # This may be the same as your `Web` server
+role :db,  "www.envaya.org", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+namespace :deploy do
+    task :finalize_update, :except => { :no_release => true } do
+        run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+        run "cp #{shared_path}/localsettings.php #{latest_release}/engine/localsettings.php"
+    end
+end
