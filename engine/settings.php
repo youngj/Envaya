0ad8a9b8256c67ad8ba554d4fc6bcc008239f8ef
Jesse Young <youngj@envaya.org>
2010-10-21 00:06:50 -0700

    only show menus on home pages in mobile view

--- engine/settings.php
+++ engine/settings.php
@@ -4,7 +4,7 @@
     global $CONFIG;
     $CONFIG = new stdClass;
 
-    $CONFIG->cache_version = 92;
+    $CONFIG->cache_version = 93;
     
     $CONFIG->dbuser = 'newslink';
     $CONFIG->dbpass = 'scarlett';
