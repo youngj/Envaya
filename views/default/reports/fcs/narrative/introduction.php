3e1248998fd670b186978f3ef1ccddb4236c6cba
Jesse Young <youngj@envaya.org>
2010-11-07 22:06:24 -0800

    hide preamble

--- views/default/reports/fcs/narrative/introduction.php
+++ views/default/reports/fcs/narrative/introduction.php
@@ -4,7 +4,7 @@ $report = $vars['report'];
 $org = $report->get_container_entity();
 
 ?>  
-<?php if (@$vars['edit']) { ?>
+<?php if (@$vars['edit'] && false) { ?>
 <div class='instructions'>
 <p><?php echo __('fcs:narrative:preamble'); ?></p>
 <p><?php echo sprintf(__('report:youare'), escape($org->name));?></p>
