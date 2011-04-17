afc9b0622624688fbbe2d22752576ab608b3d9ca
Jesse Young <youngj@envaya.org>
2011-04-17 16:46:03 -0700

    use dbname in backup script

--- scripts/backup.php
+++ scripts/backup.php
@@ -10,7 +10,8 @@
 
     $now = date("YmdHi");
     $output = "/etc/dropbox/Dropbox/envaya/envaya$now.sql.gz.nc";
-    $dump = "mysqldump envaya -u dropbox --password=''";
+    $dbname = escapeshellarg(Config::get('dbname'));
+    $dump = "mysqldump $dbname -u dropbox --password=''";
     $crypt = "mcrypt -q --key ".escapeshellarg(Config::get('dbpass'));
 
     echo system("$dump | gzip | $crypt > $output && chmod 644 $output");
