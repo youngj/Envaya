7f4b255b4261a85abeabadfe68ee9cba7c5418d1
Jesse Young <youngj@envaya.org>
2011-11-10 18:30:23 -0800

    add missing files

---
+++ views/custom/templates/layouts/default.php
@@ -0,0 +1,15 @@
+<div class="heading_container">
+    {{messages}}
+    <div class='thin_column'>{{header}}</div>
+</div>
+<div class="content_container"><div class='thin_column'>        
+        <div id='top_menu_container'>{{site_menu}}</div>
+        <div id='content'>
+            <div id='content_top'></div>
+            <div id='content_mid'>{{content}}</div>
+            <div id='content_bottom'></div>
+        </div>
+</div></div>
+<div class="footer_container"><div class='thin_column'>
+    {{footer}}
+</div></div>
