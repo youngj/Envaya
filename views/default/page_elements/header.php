19c41bc7089af68f3b07e67e2b0e0d1dc809c40f
Jesse Young <youngj@envaya.org>
2012-01-08 20:04:29 -0800

    simplify creating breadcrumb headers

--- views/default/page_elements/header.php
+++ views/default/page_elements/header.php
@@ -1,9 +1,21 @@
-<?php        
-    $title = @$vars['title'] ?: '';    
-    $escTitle = escape($title);    
-
+<?php            
     $class = @$vars['org_only'] ? "org_only_heading" : "";
 
     echo "<div id='heading'>";
-    echo "<h1 class='$class'>$escTitle</h1>";    
+    echo "<h1 class='$class'>";
+    
+    $title = @$vars['title'] ?: '';    
+    
+    if (isset($vars['header_breadcrumb_items']))
+    {
+        echo view('breadcrumb', array(
+            'items' => $vars['header_breadcrumb_items'],
+        ));
+    }
+    else
+    {    
+        echo escape($title);    
+    }
+    
+    echo "</h1>";    
     echo "</div>";
